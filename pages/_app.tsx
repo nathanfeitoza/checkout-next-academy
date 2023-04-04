e017fc851c9212cd6e2a015772181e56a9765b8a
Nathan Feitoza <nathanfeitoza@outlook.com.br>
2023-04-04 09:21:26 -0300

    adjust

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,111 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css';
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="clarity"
+        defer
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function (c, l, a, r, i, t, y) {
+          c[a] =
+            c[a] ||
+            function () {
+              (c[a].q = c[a].q || []).push(arguments);
+            };
+          t = l.createElement(r);
+          t.async = 1;
+          t.src = "https://www.clarity.ms/tag/" + i;
+          y = l.getElementsByTagName(r)[0];
+          y.parentNode.insertBefore(t, y);
+        })(window, document, "clarity", "script", "frmszwswtw");
+        `,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-S1E444HHW3"
+      />
+      <Script
+        id="gtm"
+        defer
+        strategy="afterInteractive"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        dangerouslySetInnerHTML={{
+          __html: `(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, "script", "dataLayer", "GTM-57NQJSV");`,
+        }}
+      />
+
+      <Script
+        id="gtm-trigger"
+        defer
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "G-S1E444HHW3");
+      `,
+        }}
+      />
+
+      <Script
+        id="fbPixel"
+        defer
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = "2.0";
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        "script",
+        "https://connect.facebook.net/en_US/fbevents.js"
+      );
+      fbq("init", "676029587592410");
+      fbq("track", "PageView");
+      `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- services/pixel.ts
+++ services/pixel.ts
@@ -1,6 +1,18 @@
 import ReactPixel from "react-facebook-pixel";
 
-export const Pixel = async (): Promise<typeof ReactPixel> => {
+const PixelWithScript = {
+  track: (title: string, data: any) => (window as any).fbq('track', title, data),
+  trackCustom: (event: string, data: any) => (window as any).fbq('trackCustom', event, data),
+  pageView: () => (window as any).fbq("track", "PageView"),
+}
+
+export const Pixel = async (): Promise<typeof ReactPixel | typeof PixelWithScript> => {
+  const haveFbq = (window as any).fbq;
+
+  if (haveFbq) {
+    return PixelWithScript;
+  }
+
   return new Promise((resolve) => {
     import("react-facebook-pixel")
       .then((x) => x.default)
